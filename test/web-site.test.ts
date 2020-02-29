24d055d493fd583208a86366e42df817929ece31
HeRoMo <hero@asterisk-works.jp>
2020-02-29 14:31:48 +0900

    add eslint

--- test/web-site.test.ts
+++ test/web-site.test.ts
@@ -1,13 +1,14 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
+
 import WebSite = require('../lib/web-site-stack');
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new WebSite.WebSiteStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new WebSite.WebSiteStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(matchTemplate({
+    Resources: {},
+  }, MatchStyle.EXACT));
 });
